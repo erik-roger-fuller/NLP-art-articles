bfc571dd96053150ac45a6a21b9f298978935ce5
erik.roger.fuller <erik.roger.fuller@gmail.com>
2021-06-11 13:52:57 -0400

    working thorugh the backlog

--- ner_pipeline.py
+++ ner_pipeline.py
@@ -53,12 +53,12 @@ def single_line_clean(line):
     else:
         return None
 
-def article_processor(folder_path, chunksize, begin):
+def article_processor(folder_path, chunksize, begin, u_id_begin):
     """here opens articles in chunksize chunks"""
     filelist = os.listdir(folder_path)
     jsonerror, keyerror, attribute_error,final = 0, 0, 0, 0
     # begin = 0
-    u_id = begin
+    u_id = u_id_begin
     highest = min(begin+chunksize, len(filelist))
 
     df_all = pd.DataFrame()
@@ -71,7 +71,8 @@ def article_processor(folder_path, chunksize, begin):
         f = open(filepath)  # , encoding='ascii', errors='ignore')
         try:
             j_import = json.load(f)
-            #j_import = j_import[0]
+
+            j_import = j_import[0] if type(j_import) is list else j_import
             try:
                 para = j_import['para']
                 if para is None:
@@ -83,6 +84,9 @@ def article_processor(folder_path, chunksize, begin):
                 para = np.NAN
                 keyerror += 1
                 continue
+            except TypeError:
+                print(j_import)
+                break
 
             try:
                 title = j_import['title']
@@ -150,6 +154,13 @@ def article_processor(folder_path, chunksize, begin):
             df["source"] = j_import['source']
             df["filename"] = str(file)
 
+            if j_import['source'] == 'artagenda' and title == "art-agenda":
+                name = str(j_import['url'])
+                name = name[0] if type(name) is list else name
+                name = name.split("/")[-1]
+                name = name.replace("-"," ").replace("]"," ").replace("'","")
+                df['title']= name.title()
+
             if pd.notna(author) == True:
                 df['is_author'] = df['ent_string'].where(df['ent_string'] == author.lower(), False)
                 df['is_author'] = df['is_author'].where(df['is_author'] == False, True)
@@ -180,11 +191,11 @@ def article_processor(folder_path, chunksize, begin):
     return df_all
 
 
-global_path ='C:/Users/17742/Desktop/win_art_writing/art_writing/text_cleaned'  #windows:
+global_path ='C:/Users/17742/Desktop/win_art_writing/art_writing' #text_cleaned  #windows:
 #ubuntu  : global_path = '/home/erik/Desktop/Datasets/art/art_writing/text_cleaned', nlp=nlp
 chunksize = 1500
 
-path = 'text_cleaned_all'
+path = 'artagenda' #'text_cleaned_all'
 #path = 'artnews_artinamerica'
 folder_path = os.path.join(global_path, path)
 
@@ -198,11 +209,11 @@ pd.set_option('mode.chained_assignment', None)
 
 for begin in range(0, len(filelist), chunksize): # 5 len(filelist)
     per = time.time()
-    df_all = article_processor(folder_path, chunksize, begin)
+    df_all = article_processor(folder_path, chunksize, begin, 250240 )
 
     #df_all.to_pickle(f"pkl_backups/second_pass/new_pass_first_corpus_{begin}_to_{begin+chunksize}.pkl")
     df_all = df_all.reset_index()
-    df_all.to_feather(f"pkl_backups/second_pass/new_pass_first_corpus_{begin}_to_{begin+chunksize}.feather")
+    df_all.to_feather(f"pkl_backups/second_pass/pass_art_agenda_{begin}_to_{begin+chunksize}.feather")
 
     end = time.time()
     shape = df_all.shape[0]
@@ -219,4 +230,9 @@ for begin in range(0, len(filelist), chunksize): # 5 len(filelist)
 """        text_dict = {"title": title, "article_uid": u_id, "author": author,
                      "url": j_import['url'], "tags": tags, "pubtime": pubtime,
                      "source": j_import['source'], "filename": file }
+                     
+final {'ent_string':text, 'ent_type':label, "ent_id":ent_id,  "article_uid": u_id, "author": author, "url": j_import['url'], "tags": tags,
+ "pubtime": pubtime, "source": j_import['source'], "filename": file, 'is_author':is_author,  'in_title':in_title,"polarity": sent[0] ,
+  "subjectivity": sent[1]  }         
+                     
                      """
